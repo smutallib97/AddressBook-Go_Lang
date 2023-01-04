55bb7e0f5665503bbfd4a4cb997addefcb845b78
smutallib97 <smutallib97@gmail.com>
2023-01-04 12:27:57 +0530

    Added: Ability to create contacts in addressbook

---
+++ AddressBook/UC1.go
@@ -0,0 +1,12 @@
+package main
+
+type Contact struct {
+	FirstName    string
+	LastName     string
+	Address      string
+	City         string
+	State        string
+	ZipCode      int
+	MobileNumber int
+	Email        string
+}
